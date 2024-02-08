3c81a89f84c27c0e13421f3270c35737b10b349a
sitiintannuraeni <sitiintannuraeni@smkwikrama.sch.id>
2024-02-08 23:01:27 +0700

    add route for storage:link

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/storage-link', function () {
+    \Illuminate\Support\Facades\Artisan::call('storage:link');
+    return "artisan storage:link success";
+});
